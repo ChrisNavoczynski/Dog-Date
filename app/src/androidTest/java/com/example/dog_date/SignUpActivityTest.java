7cc8616c45da4719f868d6e172352914961aa75f
Kevin Le <kevinles117@yahoo.com>
2021-11-30 19:58:46 -0800

    initial commit, build fixes

--- app/src/androidTest/java/com/example/dog_date/SignUpActivityTest.java
+++ app/src/androidTest/java/com/example/dog_date/SignUpActivityTest.java
@@ -5,16 +5,20 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.mockito.Mockito.*;
 
-import static org.hamcrest.Matchers.allOf;
+import android.content.Intent;
+import android.net.Uri;
 
-import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,14 +26,43 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 
 public class SignUpActivityTest {
+
+    private FirebaseAuth firebaseAuth;
+
+    private FirebaseUser firebaseUser;
+
     @Rule
     public ActivityScenarioRule<SignUpActivity> activityScenarioRule
             = new ActivityScenarioRule<>(SignUpActivity.class);
+
+//    @Before
+//    public void init() {
+//        firebaseAuth = mock(FirebaseAuth.class);
+//        firebaseUser = mock(FirebaseUser.class);
+//
+//        FirebaseAuthGetter.setFirebaseAuth(firebaseAuth);
+//        when(firebaseAuth.getCurrentUser()).thenReturn(firebaseUser);
+//        when(firebaseUser.getDisplayName()).thenReturn("Mock User");
+//        when(firebaseUser.getPhotoUrl()).thenReturn(Uri.parse("https://images.unsplash.com/photo-1521247560470-d2cbfe2f7b47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"));
+//        when(firebaseUser.getEmail()).thenReturn("mock@gmail.com");
+//    }
 
     @Test
     public void hasTextOnScreen() {
         onView(withId(R.id.appTitle))
                 .check(matches(withText(R.string.create_new_profile)));
+    }
+
+    @Test
+    public void signUp() {
+        onView(withId(R.id.username)).perform(typeText("MockUser"));
+        closeSoftKeyboard();
+        onView(withId(R.id.email)).perform(typeText("mock@gmail.com"));
+        closeSoftKeyboard();
+        onView(withId(R.id.password)).perform(typeText("Asdfasdf1!"));
+        closeSoftKeyboard();
+
+//        onView(withId(R.id.imageProfile)).perform(click());
     }
 
 /*    @Test
