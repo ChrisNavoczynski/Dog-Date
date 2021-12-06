b4922429e9a2d3e9c8d5cb8c2f6286d4232f9fa6
Maria Fernandez <airamreltub@gmail.com>
2021-12-05 19:55:15 -0800

    added tests to log in

--- app/src/androidTest/java/com/example/dog_date/SignUpActivityTest.java
+++ app/src/androidTest/java/com/example/dog_date/SignUpActivityTest.java
@@ -5,33 +5,51 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 import static org.mockito.Mockito.*;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-
+@LargeTest
 public class SignUpActivityTest {
 
     private FirebaseAuth firebaseAuth;
-
     private FirebaseUser firebaseUser;
+
 
     @Rule
     public ActivityScenarioRule<SignUpActivity> activityScenarioRule
