10b92df2c62a7e0898fb689a0f470d67d792a001
Maria Fernandez <airamreltub@gmail.com>
2021-12-05 18:32:36 -0800

    added testing to SignUpActivity

--- app/src/androidTest/java/com/example/dog_date/SignUpActivityTest.java
+++ app/src/androidTest/java/com/example/dog_date/SignUpActivityTest.java
@@ -5,8 +5,10 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static org.mockito.Mockito.*;
 
 import android.content.Intent;
@@ -50,4 +52,67 @@ public class SignUpActivityTest {
         onView(withId(R.id.password)).perform(typeText("Asdfasdf1!"));
         closeSoftKeyboard();
     }
+
+
+    @Test
+    public void hasUserName(){
+        onView(withId(R.id.username)).perform(typeText(""));
+        closeSoftKeyboard();
+        onView(withId(R.id.email)).perform(typeText("mock@gmail.com"));
+        closeSoftKeyboard();
+        onView(withId(R.id.password)).perform(typeText("Asdfasdf1!"));
+        closeSoftKeyboard();
+        onView(withId(R.id.b_signUp)).perform(click());
+        onView(allOf(withId(R.id.username), hasErrorText("Please Enter Username")));
+    }
+
+    @Test
+    public void hasEmail(){
+        onView(withId(R.id.username)).perform(typeText("MockUser"));
+        closeSoftKeyboard();
+        onView(withId(R.id.email)).perform(typeText(""));
+        closeSoftKeyboard();
+        onView(withId(R.id.password)).perform(typeText("Asdfasdf1!"));
+        closeSoftKeyboard();
+        onView(withId(R.id.b_signUp)).perform(click());
+        onView(allOf(withId(R.id.username), hasErrorText("Please Enter Valid Email")));
+    }
+
+
+    @Test
+    public void hasValidEmail(){
+        onView(withId(R.id.username)).perform(typeText("MockUser"));
+        closeSoftKeyboard();
+        onView(withId(R.id.email)).perform(typeText("m"));
+        closeSoftKeyboard();
+        onView(withId(R.id.password)).perform(typeText("Asdfasdf1!"));
+        closeSoftKeyboard();
+        onView(withId(R.id.b_signUp)).perform(click());
+        onView(allOf(withId(R.id.username), hasErrorText("Please Enter Valid Email")));
+    }
+
+    @Test
+    public void hasPassword(){
+        onView(withId(R.id.username)).perform(typeText("MockUser"));
+        closeSoftKeyboard();
+        onView(withId(R.id.email)).perform(typeText("mock@gmail.com"));
+        closeSoftKeyboard();
+        onView(withId(R.id.password)).perform(typeText(""));
+        closeSoftKeyboard();
+        onView(withId(R.id.b_signUp)).perform(click());
+        onView(allOf(withId(R.id.username), hasErrorText("Password must contain: At least 8 characters, 1 number, 1 special character, 1 Upper and lower case letters")));
+    }
+
+    @Test
+    public void hasValidPassword(){
+        onView(withId(R.id.username)).perform(typeText("MockUser"));
+        closeSoftKeyboard();
+        onView(withId(R.id.email)).perform(typeText("mock@gmail.com"));
+        closeSoftKeyboard();
+        onView(withId(R.id.password)).perform(typeText("A"));
+        closeSoftKeyboard();
+        onView(withId(R.id.b_signUp)).perform(click());
+        onView(allOf(withId(R.id.username), hasErrorText("Password must contain: At least 8 characters, 1 number, 1 special character, 1 Upper and lower case letters")));
+    }
+
 }
